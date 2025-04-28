a64e7b534854c9f33e95af5a1f8de4ba918ec52b
Hoàng Trang <trang010280@gmail.com>
2025-04-28 22:16:43 +0700

    setup project and add config

---
+++ src/test/java/com/hoangtrang/taskoserver/TaskoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangtrang.taskoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
